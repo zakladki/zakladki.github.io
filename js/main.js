4ef9d997255375a539b21f43e694aa9fbe3948a0
zakladki <serg21439055Z>
2019-03-18 11:13:48 +0200

    New version

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// $('.navbar-nav>li>a').on('click', function() {
+//   $('.navbar-collapse').collapse('hide');
+//   $(".hamburger").removeClass("is-active");
+//   $(".hamburger").addClass("collapsed");
+// });
+
+var forEach = function(t, o, r) {
+  if ("[object Object]" === Object.prototype.toString.call(t))
+    for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+  else
+    for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
+};
+
+var hamburgers = document.querySelectorAll(".hamburger");
+if (hamburgers.length > 0) {
+  forEach(hamburgers, function(hamburger) {
+    hamburger.addEventListener("click", function() {
+      this.classList.toggle("is-active");
+    }, false);
+  });
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
